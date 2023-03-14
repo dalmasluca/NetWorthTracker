b287af4e2c3b311f91c9276a495d4eb17c481ec2
Luca Dal Mas <luca.dalmas@icloud.com>
2023-03-14 22:08:52 +0100

    v 0.0.1 start to build the core

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"encoding/gob"
-	"log"
 	"os"
-	"time"
 )
 
-type NetWorth struct {
-	Ammount int
-	Date    time.Time
-}
-
 func main() {
-	nw := NetWorth{Ammount: 20, Date: time.Now()}
-	f, err := os.Create("file/NW.gob")
-	if err != nil {
-		log.Fatal(err)
+
+	if _, err := os.Stat("log.log"); os.IsNotExist(err) {
+		Initilization()
 	}
-	defer f.Close()
-	enc := gob.NewEncoder(f)
-	if err := enc.Encode(nw); err != nil {
-		log.Fatal(err)
-	}
+
+	MainLoop()
+
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# NetWorthTracker
+
+## cosa è
+sono ossessionato da tenr traccia delle mie spese e del mio patrimonio, ho quindi deciso di abbandonareexcell per costruire la mia personale programma di gestione delle spese. 
+
+## cercasi
+cerco qualcuno che mi aiuti a scrivere codice assieme a me
+
+
